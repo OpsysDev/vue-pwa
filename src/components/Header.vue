3ab55624e2d2f788236307add0956ba58951184a
Opsys2015 <otsuka.opsys+github@gmail.com>
2019-02-20 19:03:36 +0900

    add vuetify navber.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,36 @@
 <template>
   <div class="header">
-    <div class="h-logo">
-      <router-link to="/">
-        <img alt="works for plants" src="../assets/logo.png">
-      </router-link>
-    </div>
-    <div class="h-nav">
-      <navigation/>
-    </div>
+    <v-toolbar>
+      <div class="h-logo">
+        <router-link to="/">
+          <img alt="works for plants" src="../assets/logo.png">
+        </router-link>
+      </div>
+      <v-toolbar-title class="nav-title">Works For Plants</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <router-link to="/plants" class="nav-link"><v-btn flat>Plants</v-btn></router-link>
+        <router-link to="/month" class="nav-link"><v-btn flat>Month</v-btn></router-link>
+      </v-toolbar-items>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+    </v-toolbar>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import navigation from '@/components/Navigation.vue';
 
-@Component({
-  components: {
-    navigation
-  }
-})
+@Component
 export default class Header extends Vue {}
 </script>
 
 <style lang="scss">
-.header {
-  background: red;
-  display: flex;
-  align-items: center;
-
-  .h-logo {
-    height: 64px;
-    width: 64px;
-  }
-  .h-nav {
-    height: 100%;
-    width: 100%;
-  }
+.nav-title{
+  font-weight: bold;
+}
+.nav-link{
+  height: 100%;
+  widows: 100%;
+  text-decoration: none;
 }
 </style>
